5e7535c3b9f188790940b2b7d4ce35b0b579ccfe
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-04-13 08:02:58 +0530

    Update TB_Barrel_shifter.v

--- TB_Barrel_shifter.v
+++ TB_Barrel_shifter.v
@@ -1,32 +1,33 @@
 `timescale 1ns/1ps
 
-module tb_barrel_shifter_dir;
-//inputs and output
+module barrel_shifter_tb;
+
     reg  [7:0] data_in;
     reg  [2:0] shift;
     reg        dir;
     wire [7:0] data_out;
 
-    //module instantiation 
+    // DUT
     barrel_shifter_dir uut (
         .data_in(data_in),
         .shift(shift),
         .dir(dir),
         .data_out(data_out)
     );
-//initialize the input for FPGA and also dir and shift
+
     initial begin
+        $display("Time | dir | shift | data_in  | data_out");
+        $monitor("%4t |  %b  |   %0d   | %b | %b",
+                  $time, dir, shift, data_in, data_out);
+
         data_in = 8'b10110011;
 
-        dir = 0; shift = 2; #10;   // Left shift by 2
-        dir = 1; shift = 2; #10;   // Right shift by 2
-        dir = 0; shift = 4; #10;   // Left shift by 4
-        dir = 1; shift = 3; #10;   // Right shift by 3
-        
-        $stop;
+        dir = 0; shift = 2; #10;
+        dir = 1; shift = 2; #10;
+        dir = 0; shift = 4; #10;
+        dir = 1; shift = 3; #10;
+
+        #10 $finish;
     end
 
 endmodule
-
-
-
